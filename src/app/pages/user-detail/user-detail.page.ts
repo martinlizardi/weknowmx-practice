44931ddef51eb18320d1b0d4b2978d31da240aa5
Martin Lizardi <martin.lizardi109@gmail.com>
2024-03-13 12:45:14 -0600

    feat(user detail page): apply userListComp & change styles

--- src/app/pages/user-detail/user-detail.page.ts
+++ src/app/pages/user-detail/user-detail.page.ts
@@ -8,13 +8,20 @@ import { switchMap } from 'rxjs/operators';
 
 import { UsersService } from '@services/users.service';
 import { UserCardComponent } from '@components/user-card/user-card.component';
+import { FieldListComponent } from '@components/field-list/field-list.component';
 
 @Component({
   selector: 'app-user-detail',
   templateUrl: './user-detail.page.html',
   styleUrls: ['./user-detail.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule, UserCardComponent],
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    UserCardComponent,
+    FieldListComponent
+  ],
 })
 export class UserDetailPage {
   private route = inject(ActivatedRoute);
