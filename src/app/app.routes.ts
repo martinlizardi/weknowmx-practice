676e74768ff139fa15e97607c61f86543f9f4a8c
Martin Lizardi <martin.lizardi109@gmail.com>
2024-03-13 10:52:39 -0600

    refactor: remove unused files & move home to pages folder

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-  },
-  {
-    path: 'message/:id',
-    loadComponent: () =>
-      import('./view-message/view-message.page').then((m) => m.ViewMessagePage),
+    loadComponent: () => import('./pages/home/home.page').then((m) => m.HomePage),
   },
   {
     path: '',
