0526c9d84770d16a60884e8976a3526bc45770ed
Martin Lizardi <martin.lizardi109@gmail.com>
2024-03-15 18:48:46 -0600

    test(userItem comp): tests

--- src/app/shared/components/user-item/user-item.component.spec.ts
+++ src/app/shared/components/user-item/user-item.component.spec.ts
@@ -1,7 +1,16 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  TestBed,
+  fakeAsync,
+  tick,
+  waitForAsync,
+} from '@angular/core/testing';
+import { Router, provideRouter } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { UserItemComponent } from './user-item.component';
+import { query } from '@/testing-utils';
+import { RouterTestingHarness } from '@angular/router/testing';
 
 describe('UserItemComponent', () => {
   let component: UserItemComponent;
@@ -9,16 +18,51 @@ describe('UserItemComponent', () => {
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ UserItemComponent ],
-      imports: [IonicModule.forRoot()]
+      providers: [
+        provideRouter([{ path: '**', component: UserItemComponent }]),
+      ],
+      imports: [IonicModule.forRoot(), UserItemComponent],
     }).compileComponents();
 
     fixture = TestBed.createComponent(UserItemComponent);
     component = fixture.componentInstance;
+    component.user = {
+      id: 1,
+      fullname: 'John Doe',
+      imageurl: 'https://free-images.com/lg/aa2a/oregon_beach_sand_rocks.jpg',
+      position: '',
+    };
     fixture.detectChanges();
   }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should print avatar into ion-avatar', () => {
+    const imageDe = query(fixture, 'ion-avatar img');
+    const imageEl: HTMLElement = imageDe.nativeElement;
+
+    const srcValue = imageEl.getAttribute('src');
+
+    expect(srcValue).toBe(component.user.imageurl);
+  });
+
+  it('should print fullname into ion-label', () => {
+    const labelEl: HTMLElement = query(fixture, 'ion-label').nativeElement;
+
+    expect(labelEl.innerText).toBe(component.user.fullname);
+  });
+
+  it('should navigate to /user/:id path', fakeAsync(() => {
+    const path = `/user/${component.user.id}`;
+    const router = TestBed.inject(Router);
+
+    const itemDe = query(fixture, 'ion-item');
+    itemDe.triggerEventHandler('click', {});
+    tick();
+    fixture.detectChanges();
+
+    expect(router.url).withContext('Navigate to user detail').toBe(path);
+  }));
 });
